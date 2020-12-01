0302e419b579d8fa3a7efa7f6950a811bbe440ef
Kristina Pankova <Kristina Pankova@WIN-CUR9MCP5QTE>
2020-12-01 12:29:23 +0200

    Добавьте файлы проекта.

---
+++ MushroomsTest/MushroomsTest/mushrooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MushroomsTest
+{
+    public class mushrooms
+    {
+        public string Text { get; set; }
+        public string Detail { get; set; }
+        public string ImagePath { get; set; }
+        public Type TargetPage { get; set; }
+    }
+}
